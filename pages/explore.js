9b72d04624567d134708283fb2fc5827a35892b1
Vishesh <raizadavish9027@gmail.com>
2025-06-20 15:34:55 +0530

    First project

---
+++ pages/explore.js
@@ -0,0 +1,160 @@
+gsap.from("#m2-title",{
+    opacity: 0,
+    y: 10,
+    duration: 1,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "#m2-head",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: true,
+        // pin: true,
+    }
+})
+gsap.from("#m3-title",{
+    opacity: 0,
+    y: 10,
+    duration: 1,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "#m3-head",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: true,
+        // pin: true,
+    }
+})
+
+
+
+gsap.from("#m2-head",{
+    opacity: 0,
+    y: 10,
+    duration: 1,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "#m2-head",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: true,
+        // pin: true,
+    }
+})
+
+gsap.from("#m3-head",{
+    opacity: 0,
+    y: 10,
+    duration: 1,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "#m3-head",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: true,
+        // pin: true,
+    }
+})
+
+
+
+
+
+
+
+
+gsap.from("#m2-para",{
+    opacity: 0,
+    y: 10,
+    duration: 1,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "#m2-para",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: true,
+        // pin: true,
+    }
+})
+
+gsap.from("#m3-para",{
+    opacity: 0,
+    y: 10,
+    duration: 1,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "#m3-para",
+        start: "top 70%",
+        end: "top 50%",
+        scrub: true,
+        // pin: true,
+    }
+})
+
+
+
+
+
+
+
+
+
+gsap.from("#m2-cards-area .card",{
+    opacity: 0,
+    y: 10,
+    duration: 1,
+    rotateX:180,
+    // rotateZ:180,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "#m2-cards-area .card",
+        start: "top 70%",
+        end: "top 60%",
+        scrub: true,
+        // pin: true,
+    }
+})
+
+
+
+
+gsap.from(".lay-card",{
+    opacity: 0,
+    x: -50,
+    y:50,
+    scale:0,
+    duration: 1,
+    delay:1,
+    // rotateX:180,
+    // rotateZ:180,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: ".lay-card",
+        start: "top 56%",
+        end: "top 47%",
+        scrub: true,
+        // pin: true,
+    }
+})
+
+
+gsap.from("#m3-inner-right",{
+    opacity: 0,
+    y: 50,
+    duration: 1.2,
+    delay:1,
+    rotateX:180,
+    
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "#m3-inner-right",
+        start: "top 56%",
+        end: "top 47%",
+        scrub: true,
+        // markers:true
+        // pin: true,
+    }
+})
+
+
+
+
